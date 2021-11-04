3ccd0e2b498b1d0b8e632a1a28215bc8c1a22534
Juan Ibarra <jmibarra86@gmail.com>
2021-11-04 11:33:27 -0300

    Terminamos la estructura del sitio con el footer y los logros

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+        <LinkList>
+            <LinkColumn>
+                <LinkTitle>Mail</LinkTitle>
+                <LinkItem href="mailto:jmibarra86@gmail.com">jmibarra86@gmail.com</LinkItem>
+            </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+            <CompanyContainer>
+                <Slogan>Test slogan</Slogan>
+            </CompanyContainer>
+            <SocialContainer>
+                <SocialIcons href="https://www.linkedin.com/in/juan-manuel-ibarra-activity/">
+                    <AiFillLinkedin size="3rem" />
+                </SocialIcons>
+                <SocialIcons href="https://github.com/jmibarra/">
+                    <AiFillGithub size="3rem" />
+                </SocialIcons>
+                <SocialIcons href="https://twitter.com/jibarradev">
+                    <AiFillTwitterSquare size="3rem" />
+                </SocialIcons>
+            </SocialContainer>
+        </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
