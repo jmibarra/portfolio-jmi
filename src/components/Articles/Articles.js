6e412cb97e7a3cb53d445dc622ad3f740e708a77
Juan Ibarra <jmibarra86@gmail.com>
2024-11-24 15:22:04 -0300

    Empiezo a limpiar codigo no util o viejo

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -16,43 +16,6 @@ const Articles = () => {
 
     return (
         <Section nopadding id="articles">
-            <style>
-                {`
-                    .carousel-item img {
-                        height: 600px;
-                        width: 100%;
-                        object-fit: cover;
-                    }
-                    .carousel-control-prev-icon,
-                    .carousel-control-next-icon {
-                        height: 50px;
-                        width: 50px;
-                        background-color: rgba(15, 22, 36, 0.8);
-                        border-radius: 50%;
-                        border: none;
-                        background-image: none;
-                    }
-                    
-                    .carousel-control-prev-icon:after {
-                        content: '<';
-                        font-size: 30px;
-                        color: white;
-                    }
-                    
-                    .carousel-control-next-icon:after {
-                        content: '>';
-                        font-size: 30px;
-                        color: white;
-                    }
-                    .article-title {
-                        background-color: rgba(15, 22, 36, 0.4);
-                        padding: 0.5rem;
-                        margin: 0;
-                        color: white;
-                    }
-          
-        `}
-            </style>
             <SectionDivider />
             <SectionTitle main>Artículos recientes</SectionTitle>
             <Flex gap="middle" vertical>
